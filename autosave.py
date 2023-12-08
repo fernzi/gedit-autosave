dd46acc5ea785dc905ea87e1abfdcc1dc7809552
Fern Zapata <fern@zapata.cc>
2023-12-07 20:38:02 -0700

    Style: Use a terser licence header

--- autosave.py
+++ autosave.py
@@ -1,17 +1,7 @@
-# Copyright © 2016-2020 Fern Zapata
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Copyright © 2016-2023 Fern Zapata
+# This program is subject to the terms of the GNU GPL, version 3
+# or, at your option, any later version. If a copy of it was not
+# included with this file, see https://www.gnu.org/licenses/.
 
 import datetime
 from pathlib import Path
