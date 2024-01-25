ce8e4d6585defd6bb6c7bce64aca2a4ce0a69427
Fern Zapata <fern@zapata.cc>
2024-01-24 22:29:51 -0700

    Chore: Update copyright year

--- autosave.py
+++ autosave.py
@@ -1,4 +1,4 @@
-# Copyright © 2016-2023 Fern Zapata
+# Copyright © 2016-2024 Fern Zapata
 # This program is subject to the terms of the GNU GPL, version 3
 # or, at your option, any later version. If a copy of it was not
 # included with this file, see https://www.gnu.org/licenses/.
